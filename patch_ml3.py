cbea3009737b8862f247e0411d5d12d16a428111
Cem Akpolat <cemakpolat@Mac.fritz.box>
2026-05-08 10:26:46 +0200

    remove files

--- patch_ml3.py
+++
@@ -1,8 +0,0 @@
-with open('services/ml-service/api/ml.py', 'r') as f:
-    content = f.read()
-
-if "import time\n\n_trajectory_cache = {}" not in content:
-    content = content.replace("from engine import AnalyticsEngine", "from engine import AnalyticsEngine\nimport time\n\n_trajectory_cache = {}\n_TRAJECTORY_CACHE_TTL = 3600  # 1 hour\n")
-
-with open('services/ml-service/api/ml.py', 'w') as f:
-    f.write(content)
